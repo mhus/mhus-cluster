26695325d2d8b2e6198ed77a681cb86c4d6baaaa
Mike Hummel <mike.hummel@billingsolutions.de>
2020-03-27 12:54:19 +0100

    prepare projects

---
+++ osgi-cache-api/src/main/java/de/mhus/osgi/cache/api/CacheApi.java
@@ -0,0 +1,14 @@
+package de.mhus.osgi.cache.api;
+
+import java.util.function.Function;
+
+import javax.cache.Cache;
+import javax.cache.configuration.Configuration;
+
+public interface CacheApi {
+
+//    <K, V, C extends Configuration<K, V>> Cache<K, V> getCache(String cacheName, Class<K> keyType,
+//            Class<V> valueType, Function<Void, C> creator);
+
+    
+}
